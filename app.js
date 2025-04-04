2b744d6c4e67009aa8d2ecb7ce87f16258f1a1e6
FAguirre07 <lionelfaguirre@gmail.com>
2025-04-04 19:49:40 -0300

    two

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import session from 'express-session';
 import util from 'util';
-
+import path from 'path';
 const app = express();
 
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 app.use(session({
     secret: 'secret',
     resave: false,
